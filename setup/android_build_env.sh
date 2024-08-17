5dda3648749dde4cf2377fce4b78f870608e5bda
Tyler Daniels <81002651+ty85sr@users.noreply.github.com>
2024-08-17 14:05:09 +0000

    Update android_build_env.sh

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -16,7 +16,7 @@ DEBIAN_FRONTEND=noninteractive \
     gawk git git-lfs gnupg gperf \
     htop imagemagick libcap libelf \
     libllvm julia-llvm libgmp \
-    libmpc libmpfr pngcrush \              
+    libmpc libmpfr pngcrush  gh \              
     libtool libxml2 libxml2-utils lzop \
     maven ncftp ncurses patch patchelf \
     pkg-config android-sdk-build-tools \
@@ -25,19 +25,12 @@ DEBIAN_FRONTEND=noninteractive \
     android-partition-tools mkbootimg \
     android-sysprop ext4fs-tools \
     toybox zipalign shc img2sdat sdat2img \
-    ${PACKAGES} -y
-
-echo -e "Installing GitHub CLI"
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt update
-apt install -y gh
+    -y
 
 echo -e "Setting up udev rules for adb!"
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
-sudo chmod 644 /etc/udev/rules.d/51-android.rules
-sudo chown root /etc/udev/rules.d/51-android.rules
+curl --create-dirs -L -o ~/usr/etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo chmod 644 ~/usr/etc/udev/rules.d/51-android.rules
+sudo chown root ~/usr/etc/udev/rules.d/51-android.rules
 systemctl restart udev
 
 if [[ "$(command -v make)" ]]; then
@@ -49,8 +42,8 @@ if [[ "$(command -v make)" ]]; then
 fi
 
 echo "Installing repo"
-sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-sudo chmod a+rx /usr/local/bin/repo
+curl --create-dirs -L -o ~/usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx ~/usr/local/bin/repo
 
 git config --global user.email ty85sr@gmail.com
 git config --global user.name ty85sr
