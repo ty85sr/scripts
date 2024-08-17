974780a5c34497c850f62801d4e0a265ff51b46b
Tyler Daniels <81002651+ty85sr@users.noreply.github.com>
2024-08-17 14:08:26 +0000

    Update android_build_env.sh

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -15,23 +15,22 @@ DEBIAN_FRONTEND=noninteractive \
     ccache clang cmake curl flex g++ \
     gawk git git-lfs gnupg gperf \
     htop imagemagick libcap libelf \
-    libllvm julia-llvm libgmp \
-    libmpc libmpfr pngcrush  gh \              
-    libtool libxml2 libxml2-utils lzop \
+    libllvm julia-llvm libgmp libtool \
+    libmpc libmpfr pngcrush gh \              
+    libxml2 libxml2-utils lzop \
     maven ncftp ncurses patch patchelf \
     pkg-config android-sdk-build-tools \
     pngquant re2c rsync subversion \
     texinfo unzip w3m xsltproc zip lzip \
     android-partition-tools mkbootimg \
     android-sysprop ext4fs-tools \
-    toybox zipalign shc img2sdat sdat2img \
-    -y
+    toybox zipalign shc img2sdat sdat2img -y
 
 echo -e "Setting up udev rules for adb!"
 curl --create-dirs -L -o ~/usr/etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
 sudo chmod 644 ~/usr/etc/udev/rules.d/51-android.rules
 sudo chown root ~/usr/etc/udev/rules.d/51-android.rules
-systemctl restart udev
+restart udev
 
 if [[ "$(command -v make)" ]]; then
     makeversion="$(make -v | head -1 | awk '{print $3}')"
