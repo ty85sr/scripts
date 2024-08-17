7e6b29034f72cb100c1f3e267ef29fea574b8497
Tyler Daniels <81002651+ty85sr@users.noreply.github.com>
2024-08-17 13:00:17 +0000

    Update android_build_env.sh

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -17,7 +17,7 @@ sudo apt install software-properties-common -y
 sudo apt update
 
 # Install lsb-core packages
-sudo apt install lsb-core -y
+#sudo apt install lsb-core -y
 
 LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
@@ -32,18 +32,22 @@ elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 11" ]]; then
 fi
 
 sudo DEBIAN_FRONTEND=noninteractive \
-    apt install \
+    apt-get install \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake curl expat fastboot flex g++ \
-    g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
-    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
-    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+    gawk gcc git git-lfs gnupg gperf \
+    htop imagemagick lib32readline-dev 
+lib32z1-dev libelf-dev libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' liblz4-tool libmpc-dev libmpfr-dev libncurses5-dev \
     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
-    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+    pngquant re2c rsync schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
-    libxml-simple-perl libswitch-perl apt-utils rsync \
+    libxml-simple-perl libswitch-perl apt-utils \
     ${PACKAGES} -y
+
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb && sudo dpkg -i libtinfo5_6.4-2_amd64.deb && rm -f libtinfo5_6.4-2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb && sudo dpkg -i libncurses5_6.4-2_amd64.deb && rm -f libncurses5_6.4-2_amd64.deb
 
 echo -e "Installing GitHub CLI"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -69,3 +73,6 @@ fi
 echo "Installing repo"
 sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 sudo chmod a+rx /usr/local/bin/repo
+
+git config --global user.email ty85sr@gmail.com
+git config --global user.name ty85sr
